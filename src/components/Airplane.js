1021988dc843b150ac13a2ac599ceda510178b2e
RashidaBengali <bengali.rashida@gmail.com>
2019-05-07 16:24:23 +1000

    created Airplane component

---
+++ src/components/Airplane.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+  class Airplane extends Component {
+    render() {
+      return (
+        <h1>Airplane coming soon</h1>
+      );
+    }
+  };
+export default Airplane;
