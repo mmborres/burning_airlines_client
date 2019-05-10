8515bd665344e45627067dfca3d93c859040e25a
Mabeth <mmborres@gmail.com>
2019-05-10 22:11:12 +1000

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+//import { Redirect } from 'react-router-dom';
 import UserProfile from './UserProfile';
+//import Login from './Login';
 
 class Logout extends Component {
-constructor() {
-  super()
+  constructor() {
+    super();
+
+//const Logout = ( function() {
   UserProfile.setName('');
-  UserProfile.setUserId('');
-  UserProfile.setAdmin('')
-  window.location.href = "/";
-}
+    UserProfile.setEmail('');
+    UserProfile.setUserId(-1);
+    UserProfile.setAdmin(false);
+
+    let urlstr = window.location.href;
+    if (urlstr.includes("#")) {
+      urlstr = urlstr.split("#")[0] + "#/"
+    }
+    console.log(urlstr);
+    window.location.replace(urlstr);
+    //window.location.replace(urlstr);
+    //window.location.reload();
+
+//    return
+//  } )();
+
+  }
 
   render() {
-
-      return {
-      }
+    
+    return "/"
     }
   }
 
-  export default Logout;
+
+export default Logout;
