b164a271506528faf7f90d44603abcd28b29c1a6
Mabeth <mmborres@gmail.com>
2019-05-10 22:42:43 +1000

    Updates

---
+++ precache-manifest.add05666ea26ad4b54fd5ed35a7b52a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb42c6569cd5d8960470f455f02d06a",
+    "url": "/burning_airlines_client/index.html"
+  },
+  {
+    "revision": "a3a96800eb96787659f1",
+    "url": "/burning_airlines_client/static/css/main.02449635.chunk.css"
+  },
+  {
+    "revision": "9914673504a13f8d5a8d",
+    "url": "/burning_airlines_client/static/js/2.baacf87e.chunk.js"
+  },
+  {
+    "revision": "a3a96800eb96787659f1",
+    "url": "/burning_airlines_client/static/js/main.ddc625ef.chunk.js"
+  },
+  {
+    "revision": "54f54321c2ba1528e18d",
+    "url": "/burning_airlines_client/static/js/runtime~main.229cb3ee.js"
+  },
+  {
+    "revision": "8e412b9439f262b951626c162098b0c4",
+    "url": "/burning_airlines_client/static/media/powerpuff.8e412b94.png"
+  },
+  {
+    "revision": "a1594ce66f03da88ef89390dae05da9e",
+    "url": "/burning_airlines_client/static/media/powerpuffplane.a1594ce6.jpg"
+  }
+]);
